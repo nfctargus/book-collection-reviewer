17c3161e34fce8e6a5a07995472b55b395e44af1
Kieran Lawrence <kieran.lawrence@hotmail.com>
2023-01-26 11:06:51 +0800

    $ git config --global user.name "John Doe"

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AddItemPageComponent } from './components/pages/add-item-page/add-item-page.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { LoadingComponent } from './components/partials/loading/loading.component';
+import { SearchComponent } from './components/partials/search/search.component';
+import { CategoriesComponent } from './components/partials/categories/categories.component';
+import { TitleComponent } from './components/partials/title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomeComponent,
+    AddItemPageComponent,
+    HeaderComponent,
+    LoadingComponent,
+    SearchComponent,
+    CategoriesComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/pages/add-item-page/add-item-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-item-page',
+  templateUrl: './add-item-page.component.html',
+  styleUrls: ['./add-item-page.component.css']
+})
+export class AddItemPageComponent {
+
+}
